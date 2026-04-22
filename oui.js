3e5d78e191793ecee73fc1703f57eb4fcaaeea01
Paul Coté <paulo73170@gmail.com>
2026-04-22 14:58:26 +0200

    test

---
+++ oui.js
@@ -0,0 +1,5 @@
+// Source - https://stackoverflow.com/q/27915253
+// Posted by Trey Shaffer
+// Retrieved 2026-04-22, License - CC BY-SA 3.0
+
+window.open($(this).attr("href"), '_blank', 'modal=yes');
